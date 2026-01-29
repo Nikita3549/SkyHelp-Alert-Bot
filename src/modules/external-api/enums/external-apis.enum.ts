39735616982c87a9c17dfffb96a4dc751dfb6e71
Nikita <nikitatsarenko7@gmail.com>
2026-01-30 00:20:36 +0300

    fix(external-api): change metar_api text

--- src/modules/external-api/enums/external-apis.enum.ts
+++ src/modules/external-api/enums/external-apis.enum.ts
@@ -2,5 +2,5 @@ export enum ExternalApis {
     FLIGHT_AWARE = 'Flight Aware',
     FLIGHT_API = 'Flight IO',
     CHISINAU_AIRPORT_API = 'Chisinau Airport Api',
-    METAR_API = 'Chisinau Airport Api',
+    METAR_API = 'Metar Api',
 }
